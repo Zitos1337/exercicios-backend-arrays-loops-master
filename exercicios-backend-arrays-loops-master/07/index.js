b539c3250efdb8c0a237456bad29bd1b89c736ae
Luan Kister <112554562+Zitos1337@users.noreply.github.com>
2023-06-30 17:07:39 -0300

    Update index.js

--- exercicios-backend-arrays-loops-master/07/index.js
+++ exercicios-backend-arrays-loops-master/07/index.js
@@ -1,1 +1,11 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+
+let quantidade = 0
+
+for (let i = 0; i < nomes.length; i++) {
+    if (nomes[i] === 'a' || nomes === 'A')
+        quantidade++;
+}
+if (quantidade <= 1) {
+    console.log(nomes)
+}
