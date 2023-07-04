c3e57a6584384f195fcdb1ff9ddff23b7d4ccf67
Luan Kister <112554562+Zitos1337@users.noreply.github.com>
2023-07-04 13:55:07 -0300

    Update index.js

--- exercicios-backend-arrays-loops-master/05/index.js
+++ exercicios-backend-arrays-loops-master/05/index.js
@@ -1,1 +1,7 @@
-const original = [1, 4, 12, 21, 53, 88, 112];
+const numeros = [1, 4, 12, 21, 53, 88, 112];
+
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0) {
+        console.log(numeros[i])
+    }
+}
