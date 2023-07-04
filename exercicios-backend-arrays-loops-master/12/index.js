babea5905893a2e21c729b2c17ada9ab9f9b73a6
Luan Kister <112554562+Zitos1337@users.noreply.github.com>
2023-07-04 13:49:18 -0300

    Update index.js

--- exercicios-backend-arrays-loops-master/12/index.js
+++ exercicios-backend-arrays-loops-master/12/index.js
@@ -1,3 +1,10 @@
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+for (let i = 0; i < filaDeFora.length; i++) {
+    if (filaDeDentro.length < 5) {
+        filaDeDentro.push(filaDeFora[i])
+        filaDeFora.shift()
+    }
+}
+console.log(filaDeDentro)
